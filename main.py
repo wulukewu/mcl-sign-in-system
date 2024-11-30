e326fe52e23fa049833ca08e562e1922f94880bc
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2024-11-30 19:49:39 +0800

    disable sandbox

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def signInOut(InOrOut):
 
     # Set up ChromeDriver
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
